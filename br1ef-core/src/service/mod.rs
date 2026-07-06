57bfaa8ff9a63b9822ef1661dc5fcb1304001204
Jakob <jakob.jonasson@gmail.com>
2026-07-06 21:42:29 +0000

    refactor: split digest pipeline into build_digest returning Digest

--- br1ef-core/src/service/mod.rs
+++ br1ef-core/src/service/mod.rs
@@ -3,5 +3,5 @@ mod digest;
 mod fetch;
 
 pub use delete::delete_items;
-pub use digest::digest_items;
+pub use digest::{build_digest, digest_items};
 pub use fetch::{FetchResult, MailboxStats, fetch_items};
